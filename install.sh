a7868512a22cfbde1600d1a0af1c2d598cd929e8
Nicolas K. Ribas <nicolaskribas@gmail.com>
2021-05-24 21:15:39 -0300

    Add tmux and alacritty config files

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ dotfiles_dir=~/dotfiles
 
 ln -sf $dotfiles_dir/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf $dotfiles_dir/nvim/coc.vim ~/.config/nvim/plugin/coc.vim
+ln -sf $dotfiles_dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
+ln -sf $dotfiles_dir/.tmux.conf ~/.tmux.conf
 

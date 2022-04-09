59e41bddc227758f75b01d44cada74dd747c4b32
Nicolas K. Ribas <nicolaskribas@gmail.com>
2022-04-09 15:43:42 -0300

    Neovim mappings to use clipboard

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -12,3 +12,7 @@ vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
+-- clipboard use
+vim.api.nvim_set_keymap('', '<leader>y', "\"+y", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<leader>p', "\"+p", { noremap = true, silent = true })

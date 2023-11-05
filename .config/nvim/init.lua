c037584359c683b26f36f21945209b52bf173773
Nicolas Kolling Ribas <nicolaskribas@gmail.com>
2023-11-05 15:00:31 -0300

    neovim: add mini.nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,9 +20,9 @@ opt.linebreak = true -- use 'breakat' for determine when to wrap
 opt.splitbelow = true
 opt.splitright = true
 opt.pumheight = 10
-opt.completeopt = { "menuone", "noinsert", "noselect" } -- TODO
+opt.completeopt = { "menuone", "noinsert", "noselect" }
+opt.shortmess:append "c"
 opt.wildmode = { "longest:full:lastused", "full" } -- complete until longest common string, then iterate over other matches, sort buffers by last used
-opt.wildoptions:remove "pum"
 opt.path:append "**" -- recursive :find
 opt.grepprg = "rg --smart-case --vimgrep"
 opt.grepformat:prepend "%f:%l:%c:%m"
@@ -31,8 +31,6 @@ vim.diagnostic.config {
 	virtual_text = false,
 	severity_sort = true,
 }
-
-vim.cmd "colorscheme rose-pine"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -64,7 +62,13 @@ map({ "n", "x" }, "gk", "k")
 map("n", "<Leader>d", vim.diagnostic.open_float)
 map("n", "[d", vim.diagnostic.goto_prev) -- *
 map("n", "]d", vim.diagnostic.goto_next) -- *
-map("n", "<Leader>qd", vim.diagnostic.setqflist) -- mnemonic: quickfix diagnostics
+map("n", "<Leader>qd", vim.diagnostic.setqflist)
+map("n", "<Leader>qe", function()
+	vim.diagnostic.setqflist { severity = vim.diagnostic.severity.ERROR }
+end)
+map("n", "<Leader>qw", function()
+	vim.diagnostic.setqflist { severity = vim.diagnostic.severity.WARN }
+end)
 map("n", "]q", "<Cmd>cnext<CR>")
 map("n", "[q", "<Cmd>cprev<CR>")
 map("n", "]l", "<Cmd>lnext<CR>")
@@ -73,7 +77,7 @@ map("n", "]b", "<Cmd>bnext<CR>")
 map("n", "[b", "<Cmd>bprev<CR>")
 
 map("n", "<Leader>lg", function() -- loclist grep
-	vim.ui.input({ prompt = "rg args: " }, function(args)
+	vim.ui.input({ prompt = "rg --smart-case " }, function(args)
 		if args ~= nil and args ~= "" then
 			vim.cmd.lgrep { args, mods = { silent = true } }
 			vim.cmd.lopen()
@@ -93,9 +97,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank { on_visual = false }
 	end,
 })
+
+for _, v in ipairs {
+	"textDocument/implementation",
+	"textDocument/references",
+	"textDocument/documentSymbol",
+} do
+	vim.lsp.handlers[v] = vim.lsp.with(vim.lsp.handlers[v], {
+		on_list = function(options)
+			vim.fn.setloclist(0, {}, " ", options)
+			vim.api.nvim_command "lopen"
+		end,
+	})
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = init,
 	callback = function(args)
+		vim.bo[args.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
+
 		local lsp = vim.lsp.buf
 		local opts = { buffer = args.buf }
 		map("n", "K", lsp.hover, opts) -- **
@@ -105,11 +125,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		map({ "n", "x" }, "<Leader>ca", lsp.code_action, opts)
 		map({ "n", "x" }, "<Leader>cf", lsp.format, opts)
 		map("n", "<Leader>cw", lsp.rename, opts)
-		map("n", "<Leader>qs", lsp.document_symbol, opts)
-		map("n", "<Leader>qm", lsp.implementation, opts)
-		map("n", "<Leader>qi", lsp.incoming_calls, opts)
-		map("n", "<Leader>qo", lsp.outgoing_calls, opts)
-		map("n", "<Leader>qr", lsp.references, opts)
+		map("n", "<Leader>ls", lsp.document_symbol, opts)
+		map("n", "<Leader>li", lsp.implementation, opts)
+		map("n", "<Leader>lr", lsp.references, opts)
 		map("n", "<Leader>wa", lsp.add_workspace_folder, opts)
 		map("n", "<Leader>wr", lsp.remove_workspace_folder, opts)
 		map("n", "<Leader>wl", function()
@@ -142,6 +160,21 @@ require("nvim-treesitter.configs").setup {
 	parser_install_dir = vim.fn.stdpath "data" .. "/site",
 	highlight = { enable = true },
 	indent = { enable = true },
+}
+
+require("mini.comment").setup {}
+require("mini.completion").setup {
+	lsp_completion = {
+		source_func = "omnifunc",
+		auto_setup = false,
+	},
+	set_vim_settings = false,
+}
+require("mini.hues").setup {
+	n_hues = 2,
+	background = "#2B2B2B",
+	foreground = "#A9B7C6",
+	accent = "purple",
 }
 
 -- * overrides a default keymap
